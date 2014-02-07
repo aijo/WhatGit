53d76aa2d2fd9db3e55a19f9a7282ed29e1bcbc8
Chanintorn Asavavichairoj <aijojoe@gmail.com>
2014-02-07 23:47:10 +0700

    Integrated AFNetworking dependence

---
+++ Podfile
@@ -0,0 +1,8 @@
+def import_pods
+  pod 'AFNetworking', '~> 2.0.3'
+end
+
+target :WhatGit do
+  platform :ios, '6.0'
+  import_pods
+end
---
+++ Pods/Pods-WhatGit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WhatGit : NSObject
+@end
+@implementation PodsDummy_Pods_WhatGit
+@end
