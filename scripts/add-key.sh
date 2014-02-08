04c395243bc5345fa0d8b71241184af548d68022
Chanintorn Asavavichairoj <aijojoe@gmail.com>
2014-02-08 11:43:25 +0700

    try

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -1,7 +1,3 @@
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/profile/adhoc.mobileprovision.enc -d -a -out scripts/profile/adhoc.mobileprovision
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/dist.cer.enc -d -a -out scripts/certs/dist.cer
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/dist.p12.enc -d -a -out scripts/certs/dist.p12
-
 security create-keychain -p travis ios-build.keychain
 security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
